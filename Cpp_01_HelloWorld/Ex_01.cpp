df308162d196524b13d6c5a025a3b011fcf40697
r.pouya <rpouya@hotmail.com>
2025-01-25 10:45:40 +0330

    make it better

---
+++ Cpp_01_HelloWorld/Ex_01.cpp
@@ -0,0 +1,12 @@
+
+// Ex1_01.cpp - A complete C++ program
+//import std; // This line makes the entire Standard Library available,
+#include <iostream>
+#include <print>
+// including the std::println() functionality used below
+int Ex_01()
+{
+    int answer{ 42 }; // Defines the variable answer with value 42
+    std::println("The answer to life, the universe, and everything is {}.", answer);
+    return 0;
+}
--- Cpp_01_HelloWorld/main.cpp
+++ Cpp_01_HelloWorld/main.cpp
@@ -1,33 +1,23 @@
 // Cpp_01_HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-//
-//#include <iostream>
-//#include "Logger.h"
-//#include "Math.h"
-//#define INTEGER int 
-
-//INTEGER main()
-//{
-//    Log("Hello World!");
-//    int i = 10 ;
-//    unsigned int j = 5; 
-//    Log(Multiply(i, j));
-//    std::cin.get();
-//}
-////#include "EndBrace.h"
-//
 
 
-
-
-// Ex1_01.cpp - A complete C++ program
-//import std; // This line makes the entire Standard Library available,
 #include <iostream>
-#include <print>
-// including the std::println() functionality used below
-int main()
+#include "Logger.h"
+#include "Math.h"
+#include "./Ex_01.h"
+#define INTEGER int 
+
+INTEGER main()
 {
-    int answer{ 42 }; // Defines the variable answer with value 42
-    std::println("The answer to life, the universe, and everything is {}.", answer);
-    return 0;
+    Log("Hello World!");
+    int i = 10 ;
+    unsigned int j = 5; 
+    Log(Multiply(i, j));
+    Ex_01();
+    std::cin.get();
 }
+//#include "EndBrace.h"
+
+
+
+
