fd970be395361f589d693ef3c3fef0180bb6df13
r.pouya <rpouya@hotmail.com>
2025-01-25 15:52:12 +0330

    excercise 1

--- Cpp_01_HelloWorld/Ex_01.cpp
+++ Cpp_01_HelloWorld/Ex_01.cpp
@@ -4,9 +4,35 @@
 #include <iostream>
 #include <print>
 // including the std::println() functionality used below
+
+
 int Ex_01()
 {
-    int answer{ 42 }; // Defines the variable answer with value 42
-    std::println("The answer to life, the universe, and everything is {}.", answer);
-    return 0;
+	int answer{ 42 }; // Defines the variable answer with value 42
+	std::println("The answer to life, the universe, and everything is {}.", answer);
+
+	// In general, you can use any number of replacement fields, and you can add format specifiers to the
+	// replacement fields to override the default formatting scheme.
+	// {:!^12b} In reality, this format specifier instructs std::println() to output answer as a binary number(b), centered(^)
+	// in a field that is 12 characters wide, filling the remainder of the thus overly wide field with exclamation
+	// marks(!).
+	std::println("The answer to {} is {:!^12b}", "the ultimate question", answer);
+	std::println("The answer to {} is {:b}", "the ultimate question", answer);
+
+	std::print("The answer to life, ");
+	std::print("the universe, ");
+	std::print("and everything ");
+	std::println("is {}", answer);
+
+	// Unlike in several other programming languages, you cannot invoke std::print() or
+	// std::println() without a format string.That is, you cannot, for instance, use std::println(value) to
+	// output only the value of answer.Instead, you use statements of the form std::println("{}", value).You
+	// also cannot invoke std::println() without an argument to output a line break, and nothing else.Instead,
+	// you use either std::println("") or std::print("\n").
+
+	return 0;
 }
+
+
+
+
--- enc_temp_folder/f2b85b9a8d1682c335813538e4c7d15f/main.cpp
+++ enc_temp_folder/f2b85b9a8d1682c335813538e4c7d15f/main.cpp
@@ -11,26 +11,14 @@ using namespace std;
 //Caution The main() function must not be defined within a namespace.Things that are not defined in a
 // namespace exist in the global namespace, which has no name.
 
-// Other identifiers that you are not supposed to use include the following :
-// • Identifiers that begin with two consecutive underscores
-// • Identifiers that begin with an underscore followed by an uppercase letter
-// • Within the global namespace, all names that begin with an underscore
-
 INTEGER main()
 {
     Log("Hello World!");
-    
     int i = 10 ;
-    
     unsigned int j = 5; 
-    
     Log(Multiply(i, j));
-    
     Ex_01();
-
     //std::cin.get();
-    
-    
     cin.get();
 }
 //#include "EndBrace.h"
