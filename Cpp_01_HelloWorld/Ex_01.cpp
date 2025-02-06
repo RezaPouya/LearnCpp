8abb42457c251c0c1c73bfc2e4acc4ca6ea0e610
r.pouya <rpouya@hotmail.com>
2025-02-06 12:50:50 +0330

    update

--- Cpp_01_HelloWorld/Ex_01.cpp
+++ Cpp_01_HelloWorld/Ex_01.cpp
@@ -42,6 +42,27 @@ void Ex_02_Variables() {
 	std::println("The value of apple_count is {}", apple_count);
 	std::println("The value of orange_count is {}", orange_count);
 	std::println("The value of total_fruit is {}", total_fruit);
+
+	unsigned int yards{}, feet{}, inches{};
+	// Convert a distance in yards, feet, and inches to inches
+	std::print("Enter a distance as yards, feet, and inches ");
+	std::println("with the three values separated by spaces: ");
+	std::cin >> yards >> feet >> inches;
+	const unsigned feet_per_yard{ 3 };
+	const unsigned inches_per_foot{ 12 };
+	unsigned total_inches{};
+	total_inches = inches + inches_per_foot * (yards * feet_per_yard + feet);
+	std::println("This distance corresponds to {} inches.", total_inches);
+	// Convert a distance in inches to yards, feet, and inches
+	std::print("Enter a distance in inches: ");
+	std::cin >> total_inches;
+	
+	feet = total_inches / inches_per_foot;
+	inches = total_inches % inches_per_foot;
+	yards = feet / feet_per_yard;
+	feet = feet % feet_per_yard;
+	std::println("This distance corresponds to {} yards {} feet {} inches.",
+		yards, feet, inches);
 }
 
 
