d381811ea6a87877179b7bd2575e6e991b9748e8
RezaPouya <rpouya@hotmail.com>
2025-08-16 20:02:57 +0330

    pure function is completed

--- src/Interfaces/main.cpp
+++ src/Interfaces/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
 
-class Entity {
+class IPrintable {
 public:
+	virtual ~IPrintable() = default;               // Good practice: virtual destructor
+	virtual std::string GetClassName() const = 0;  // Pure virtual function , when use ( = 0 ) , it should be overrided in sub-class
+};
+
+
+class Entity : public IPrintable {
+
+public:
+	
 	float X;
 	float Y;
+	
 	explicit Entity(float x, float y) : X(x), Y(y) {}
 
 	virtual std::string GetName() const { return "Entity"; }
@@ -13,21 +23,35 @@ public:
 		X = x * speed;
 		Y = y * speed;
 	}
+
+	std::string GetClassName() const override {
+		return "Entity";
+	}
 };
 
-class Player : public Entity {
+class Player : public Entity , public IPrintable {
 
 public:
 	const std::string Name;
+
 	Player(const std::string& name, float x, float y) : Entity(x, y), Name(name) {}
 
 	std::string GetName() const override { return this->Name; }
+
+	std::string GetClassName() const override {
+		return "Player";
+	}
 };
 
 // pure virtual function allowed us to defined a function without body in base class which should be implemented in 
 // derived classes 
 int main()
 {
+	Entity entity(12.4f, 5.4f);
+	std::cout << entity.GetClassName() << std::endl;  // Output: "Entity"
+
+	Player player("Reza" , 12.4f, 5.4f);
+	std::cout << player.GetClassName() << std::endl;  // Output: "Entity"
 	std::cin.get();
 }
 
--- src/Statics/Main.cpp
+++ src/Statics/Main.cpp
@@ -1,57 +1,29 @@
 #include <iostream>
+#define Log(x) std::cout << x << std::endl ; 
 
-
+// 
 // int s_Variable = 5;
-const extern int s_Variable; // external linking its just 
+extern int s_Variable; // external linking its just 
 
 void Function() {
-    return;
-};
 
-struct Entity {
-    static int x;
-    static int y;
-    int a;
-    int b;
-
-    void print(const char* entityName) {
-        std::cout << entityName << ": " << Entity::x << "," << Entity::y << std::endl;
-    }
-};
-
-// in C++, static member variables must be both declared inside the class and outside of it.
-int Entity::x = 0;  // Must appear exactly once in the program
-int Entity::y = 0;
-
-
-static void print_static(const Entity& entity) {
-    std::cout << "Static Call" << ": " << entity.a << "," << entity.b << std::endl;
 }
 
-// try to  mark your function and variable as static unless you want to link them across translation units
+// try to  mark your function and variable as static unless you want to link them accross translation units
 int main()
 {
-
     std::cout << s_Variable << std::endl;
-    Entity entity01;
-    entity01.a = 5;
-    entity01.b = 10;
-    Entity::x = 14;
-    Entity::y = 72; 
-
-    //Entity entity02 = {  3 , 4 };
-
-    Entity entity02;
-    entity02.a = 12;
-    entity02.b = 87;
-    Entity::y = 32;
-
-    entity01.print("entity-01");
-    entity02.print("entity-02");
-    print_static(entity01);
-    print_static(entity02);
-
+    std::cout << "Hello World!\n";
     std::cin.get();
 }
 
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
