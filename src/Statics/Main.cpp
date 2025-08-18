ade89c0cf6743a8c2701a70625e38dcb82288bbb
RezaPouya <rpouya@hotmail.com>
2025-08-18 22:55:50 +0330

    local statics

--- src/Statics/Main.cpp
+++ src/Statics/Main.cpp
@@ -1,19 +1,82 @@
 #include <iostream>
-#define Log(x) std::cout << x << std::endl ; 
+#include <iostream>
+#include <mutex>
+
 
 // 
 // int s_Variable = 5;
 extern int s_Variable; // external linking its just 
 
 void Function() {
-
+    static int i = 0; // only initialize in first run 
+    i++;
+    std::cout << i << std::endl;
 }
 
-// try to  mark your function and variable as static unless you want to link them accross translation units
+// old way
+//class SingletonLogger {
+//private:
+//    static SingletonLogger* s_instance;
+//    int m_logCounterCall;
+//
+//public:
+//    SingletonLogger() : m_logCounterCall(0) {}
+//
+//    static SingletonLogger& Get() {
+//        if (!s_instance) {
+//            s_instance = new SingletonLogger();  // Initialize if null
+//        }
+//        return *s_instance;
+//    }
+//
+//    void Write(const char* message)  {
+//        m_logCounterCall++;
+//        std::cout << "[LOG]: " << message << std::endl;
+//    }
+//    int GetCountOfCall() {
+//        return m_logCounterCall;
+//    }
+//};
+//
+//SingletonLogger* SingletonLogger::s_instance = nullptr;
+
+class SingletonLogger {
+private:
+    int m_logCounterCall;
+    SingletonLogger() : m_logCounterCall(0) {}
+
+public:
+    static SingletonLogger& Get() {
+        static SingletonLogger logger; // local static 
+        return logger;
+    }
+
+    void Write(const char* message) {
+        m_logCounterCall++;
+        std::cout << "[LOG]: " << message << std::endl;
+    }
+
+    int GetCountOfCall() {
+        return m_logCounterCall;
+    }
+};
+
+// try to  mark your function and variable as static unless you want to link them across translation units
 int main()
 {
     std::cout << s_Variable << std::endl;
-    std::cout << "Hello World!\n";
+    SingletonLogger::Get().Write("1 log");
+    SingletonLogger::Get().Write("2 log");
+    SingletonLogger::Get().Write("3 log");
+    SingletonLogger::Get().Write("4 log");
+    std::cout << "[LOG-Counter]: " << SingletonLogger::Get().GetCountOfCall() << std::endl;
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
+    Function();
     std::cin.get();
 }
 
