90686e749a172f27279a761992bf28e3a2f8cfe1
r.pouya <rpouya@hotmail.com>
2025-08-19 13:30:39 +0330

    reutnr multiple Item

---
+++ src/ReturnMultipleItemFromFunction/ReturnMultipleItemFromFunction.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <tuple>
+
+struct ResultDto {
+    std::string FirstName = {};
+    std::string LastName = {};
+
+    ResultDto(std::string fn, std::string ln) : FirstName(fn), LastName(ln) {}
+};
+
+std::tuple<std::string, std::string> ReturnFromTuple() {
+
+    std::string fName("Reza");
+    std::string lName("Pouya");
+
+    return std::make_pair(fName, lName);
+}
+
+std::pair<std::string, std::string> ReturnFromPair() {
+
+    std::string fName("Reza");
+    std::string lName("Pouya");
+
+    return std::make_pair(fName, lName);
+}
+
+const ResultDto* ReturnStruct()  {
+
+    return new ResultDto(std::string("Reza"), std::string("Pouya"));
+
+}
+
+int main()
+{
+    {
+        auto result = ReturnFromTuple();
+
+        auto fname = std::get<0>(result);
+        auto lname = std::get<0>(result);
+
+        std::cout << fname << " " << lname;
+    }
+    
+    {
+        auto result = ReturnFromPair();
+
+        std::cout << result.first << " " << result.second;
+    }
+
+
+    {
+        auto result = ReturnStruct();
+
+        std::cout << result->FirstName << " " << result->LastName;
+    }
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
