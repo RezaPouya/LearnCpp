2c8ba881d3fd0f86d142e17ddd58ab27ca79a1fe
r.pouya <rpouya@hotmail.com>
2025-08-20 13:42:06 +0330

    initial command manager is build

--- examples/ToDoApp/ToDoApp/Application.cpp
+++ examples/ToDoApp/ToDoApp/Application.cpp
@@ -3,20 +3,18 @@
 //$(SolutionDir)bin\examples\to-do-app\$(ProjectName)\$(Platform)\$(Configuration)\
 
 
-#include <iostream>
+#include "Manages/CommandManager.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// use pointer to function to register all commands from ToDoTaskManager to CommandManagers
+int main() {
+    CommandManager app;
+
+    // You can override default handlers:
+    app.registerCommandHandler(MenuCommand::Add, []() {
+        std::cout << "Custom add handler called!\n";
+        // Implement actual add logic here
+    });
+
+    app.run();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
